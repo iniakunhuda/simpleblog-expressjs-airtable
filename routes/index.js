230edb60563f1819aace3245602032ac24098e56
Miftahul Huda <iniakunhuda@gmail.com>
2020-05-16 10:34:03 +0700

    update template

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ const app = express();
 const airService = require('../services/airtable-api');
 const airAPI = new airService();
 
-var articles = []
 
 app.get('/', async (req, res) => {
+    var articles = []
     res.render('index', {articles: articles});
 })
 
 app.get('/detail/(:slug)', async (req, res) => {
+    var article = {}    
     res.render('detail', {article: article}); //
 })
 
